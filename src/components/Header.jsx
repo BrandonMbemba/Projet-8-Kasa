ddab787259e25f9981667c597018123a20e3a0fb
Brandon M'bemba <brandonmbemba@gmail.com>
2024-03-06 15:57:32 +0100

    Nav Link configured

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <div className="header">
             <img src="./images/LOGO.svg"></img>
             <nav>
-            <ul>Acceuil</ul>
-            <ul>A propos</ul>
+                <ul>
+                <NavLink to = "/" className={({isActive}) => (isActive ? "underline" : "")}>
+                    <li>Acceuil</li>
+                </NavLink>
+                <NavLink to = "/about" className={({isActive}) => (isActive ? "underline" : "")}>
+                    <li>A propos</li>
+                </NavLink>
+                </ul>
             </nav>
         </div>
     );  
